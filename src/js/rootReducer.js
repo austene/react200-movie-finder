88a1332ebf82eedd84a74819000a69c6fd1bf0f9
AE <austence@sbcglobal.net>
2019-05-15 15:58:55 -0700

    initial commit

---
+++ src/js/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import SearchbarReducer from './components/SearchBar/searchbarReducer';
+
+const rootReducer = combineReducers({
+  // searchbar: SearchbarReducer,
+});
+
+export default rootReducer;
